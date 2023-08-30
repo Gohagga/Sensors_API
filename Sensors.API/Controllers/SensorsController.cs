d47b3f4c97fa7f25fb1e76fd7e6e7146c33ca274
Bartul Grbin <bartul.grbin@gdi.net>
2023-08-30 17:01:20 +0200

    Add project files.

---
+++ Sensors.API/Controllers/SensorsController.cs
@@ -0,0 +1,92 @@
+using Microsoft.AspNetCore.Mvc;
+using Sensors.API.Models;
+
+namespace Sensors.API.Controllers
+{
+    [ApiController]
+    [Route("sensors")]
+    public class SensorsController : ControllerBase
+    {
+        private readonly ILogger<SensorsController> _logger;
+        private static List<SensorInfo>? _sensorInfos;
+
+        private static Dictionary<string, SensorData>? _sensorData;
+
+        public SensorsController(ILogger<SensorsController> logger)
+        {
+            _logger = logger;
+            _sensorInfos ??= Enumerable.Range(1, 5).Select(index =>
+                new SensorInfo(
+                    Id: Guid.NewGuid().ToString(),
+                    Label: $"SENZOR{index}"))
+                .ToList();
+
+            _sensorData ??= _sensorInfos.ToDictionary(k => k.Id, v => new SensorData(
+                SensorId: v.Id,
+                HitsPerPeriod: Enumerable.Range(0, 10000).Select(y => Random.Shared.Next(0, 40)).ToList()));
+        }
+
+        [HttpGet("get-sensors")]
+        public ActionResult<GetSensorsResponse> GetSensors()
+        {
+            var retVal = new GetSensorsResponse();
+            retVal.AddRange(_sensorInfos);
+
+            return retVal;
+        }
+
+        [HttpPost("get-sensor-data")]
+        public ActionResult<GetSensorDataResponse> GetSensorData([FromBody] GetSensorDataRequest request)
+        {
+            var retVal = new GetSensorDataResponse();
+            
+            foreach (var sensorId in request.SensorsToReturn)
+            {
+                var data = _sensorData[sensorId];
+
+                var hitsPerPeriod = request.Period switch
+                {
+                    TimePeriod.Minute => data.HitsPerPeriod,
+                    TimePeriod.Hour => SumEveryN(data.HitsPerPeriod, 60),
+                    TimePeriod.Day => SumEveryN(data.HitsPerPeriod, 1440),
+                    TimePeriod.Month => SumEveryN(data.HitsPerPeriod, 43200),
+                    TimePeriod.Quarter => SumEveryN(data.HitsPerPeriod, 129600),
+                    _ => data.HitsPerPeriod
+                };
+
+                var sensorData = new SensorData(
+                    SensorId: data.SensorId,
+                    HitsPerPeriod: hitsPerPeriod);
+
+                retVal.Add(sensorData);
+            }
+
+            return retVal;
+        }
+
+        private static List<int> SumEveryN(List<int> data, int n)
+        {
+            List<int> result = new List<int>();
+            int sum = 0;
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                sum += data[i];
+
+                if ((i + 1) % n == 0)
+                {
+                    result.Add(sum);
+                    sum = 0;
+                }
+            }
+
+            // If there's a remainder, add it to the results
+            if (data.Count % n != 0)
+            {
+                result.Add(sum);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Sensors.API/Models/SensorsDTO.cs
@@ -0,0 +1,35 @@
+﻿namespace Sensors.API.Models
+{
+    // Requests
+    public record GetSensorDataRequest(
+        DateTime From,
+        DateTime To,
+        TimePeriod Period,
+        List<string> SensorsToReturn,
+        bool PerfectlyPredictedData = false);
+
+    
+    // Responses
+    public class GetSensorsResponse : List<SensorInfo> { };
+
+    public class GetSensorDataResponse : List<SensorData> { };
+
+
+    // Models
+    public record SensorInfo(
+        string Id,
+        string Label);
+
+    public record SensorData(
+        List<int> HitsPerPeriod,
+        string SensorId);
+
+    public enum TimePeriod
+    {
+        Minute = 0,
+        Hour = 1,
+        Day = 2,
+        Month = 3,
+        Quarter = 4,
+    }
+}
